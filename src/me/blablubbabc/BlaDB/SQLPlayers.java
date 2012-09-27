ec233b1f5efea167d7139f3e195c418aaccac8de
blablubbabc <lukas@wirsindwir.de>
2012-09-27 02:35:03 +0200

    Added SQLPlayers

---
+++ src/me/blablubbabc/BlaDB/SQLPlayers.java
@@ -0,0 +1,23 @@
+package me.blablubbabc.BlaDB;
+
+import me.blablubbabc.paintball.Paintball;
+
+public class SQLPlayers {
+
+	private static BlaSQLite sql;
+	private static Paintball plugin;
+
+	public SQLPlayers(BlaSQLite blasql, Paintball pl) {
+		sql = blasql;
+		plugin = pl;
+	}
+
+	//PLAYERDATA
+	//GET
+
+	//SET
+	
+	//REMOVE
+
+	//ADD NEW
+}
