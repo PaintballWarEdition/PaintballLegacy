2d505e43d5f650a3580fe53062fdb1c53f447da6
blablubbabc <lukas@wirsindwir.de>
2013-02-05 23:15:40 +0100

    falling blocks..

--- src/me/blablubbabc/paintball/extras/Grenade.java
+++ src/me/blablubbabc/paintball/extras/Grenade.java
@@ -2,6 +2,8 @@ package me.blablubbabc.paintball.extras;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
+
 import me.blablubbabc.paintball.Paintball;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -14,6 +16,7 @@ import org.bukkit.util.Vector;
 
 public class Grenade{
 	
+	private static Random random = new Random();
 	private static HashMap<Player, ArrayList<Egg>> nades = new HashMap<Player, ArrayList<Egg>>();
 	
 	public static synchronized void eggThrow(Player player, Egg nade) {
@@ -50,20 +53,26 @@ public class Grenade{
 	public static void explodeBlocks(Player player, Projectile nade, Paintball plugin, Material mat) {
 		Location loc = nade.getLocation();
 		loc.getWorld().createExplosion(loc, -1.0F);
+		
+		int i;
 		for(Vector v : directions()) {
-			FallingBlock f = loc.getWorld().spawnFallingBlock(loc, mat, (byte)0);
-			FallingBlocks.addFallingBlock(f);
-			moveBlock(f, v, plugin);
+			i = random.nextInt(10);
 			moveExpSnow(loc.getWorld().spawn(loc, Snowball.class), v, player, plugin);
+			if(i < 5) {
+				FallingBlock f = loc.getWorld().spawnFallingBlock(loc, mat, (byte)0);
+				f.setDropItem(false);
+				FallingBlocks.addFallingBlock(f);
+				moveBlock(f, v, plugin);
+			}
 		}
 	}
 	
 	private static void moveBlock(final FallingBlock f, Vector v, Paintball plugin) {
-		Vector v2 = v;
+		Vector v2 = v.clone();
 		v2.setX(v.getX()+ Math.random()- Math.random());
 		v2.setY(v.getY()+ Math.random()- Math.random());
 		v2.setZ(v.getZ()+ Math.random()- Math.random());
-		f.setVelocity(v2.multiply(1));
+		f.setVelocity(v2.multiply(0.5));
 		plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
 			
 			@Override
@@ -75,7 +84,7 @@ public class Grenade{
 	
 	private static void moveExpSnow(final Snowball s, Vector v, Player player, Paintball plugin) {
 		s.setShooter(player);
-		Vector v2 = v;
+		Vector v2 = v.clone();
 		v2.setX(v.getX()+ Math.random()- Math.random());
 		v2.setY(v.getY()+ Math.random()- Math.random());
 		v2.setZ(v.getZ()+ Math.random()- Math.random());
