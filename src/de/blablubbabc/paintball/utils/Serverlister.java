b2f678c3b0bc83b77ff20fc8132bf4a47479488e
blablubbabc <lukas@wirsindwir.de>
2013-06-13 15:08:56 +0200

    ..

--- src/de/blablubbabc/paintball/utils/Serverlister.java
+++ src/de/blablubbabc/paintball/utils/Serverlister.java
@@ -2,37 +2,34 @@ package de.blablubbabc.paintball.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.UUID;
 
 import org.bukkit.configuration.file.YamlConfiguration;
 
 public class Serverlister {
 	private final static String CONFIG_FILE = "plugins/Paintball/serverlist.yml";
-	private final YamlConfiguration defcon;
+	private final YamlConfiguration defaults;
 	private final YamlConfiguration config;
 	private final File configFile;
 	//private static final int PING_INTERVAL = 10;//unused
 	//CONFIG VALUES
-	private final HashMap<String, String> vars;
 	
 	public Serverlister() {
-		this.defcon = new YamlConfiguration();
-		this.vars = new HashMap<String, String>();
+		this.defaults = new YamlConfiguration();
 		// load the config
         configFile = new File(CONFIG_FILE);
         config = YamlConfiguration.loadConfiguration(configFile);
-		//WERTE MIT DEFAULTS
-		setDefcon();
-		//NEUE WERTE IN DIE CONFIG SCHREIBEN
-		for(String node : defcon.getValues(true).keySet()) {
+		// init defaults:
+		initDefaults();
+		// set defaults:
+		for(String node : defaults.getValues(true).keySet()) {
 			//CONFIG
-			if(config.get(node) == null) config.set(node, defcon.get(node));
+			if(config.get(node) == null) config.set(node, defaults.get(node));
 		}
 		//Korrektur:
 		String serverid = config.getString("Server.Id");
 		if(!isValid(serverid)) {
-			config.set("Server.Id", defcon.get("Server.Id"));
+			config.set("Server.Id", defaults.get("Server.Id"));
 		}
 		try {
 			config.save(configFile);
@@ -42,21 +39,13 @@ public class Serverlister {
 		
 	}
 	
-	private void setDefcon() {
-		//VALUES (confignode, default, var)
-		addValue("Server.Id", UUID.randomUUID().toString(), "serverid");
-		addValue("Server.List", true, "serverlist");
+	private void initDefaults() {
+		defaults.set("Server.Id", UUID.randomUUID().toString());
+		defaults.set("Server.List", true);
 	}
 	
-	private void addValue(String node, Object def, String var) {
-		//VALUES
-		defcon.set(node, def);
-		//ZUORDNUNG
-		vars.put(var, node);
-	}
-	
-	public Object get(String var) {
-		return config.get(vars.get(var), defcon.get(vars.get(var)));
+	public Object get(String node) {
+		return config.get(node, defaults.get(node));
 	}
 	
 	
