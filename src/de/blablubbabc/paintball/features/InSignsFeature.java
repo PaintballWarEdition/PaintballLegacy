ac8dcac63934090c6cbac19995fa0c0df45e1b04
blablubbabc <lukas@wirsindwir.de>
2013-06-15 01:19:16 +0200

    Adding insiugns rank signs. CHanging insigns permissions to paintball.admin

--- src/de/blablubbabc/paintball/features/InSignsFeature.java
+++ src/de/blablubbabc/paintball/features/InSignsFeature.java
@@ -13,20 +13,22 @@ import org.bukkit.plugin.Plugin;
 public class InSignsFeature {
 	private final Paintball plugin;
 	private final InSigns insigns;
+	private final DecimalFormat format;
 	
 	public InSignsFeature(Plugin insignsPlugin, Paintball pbPlugin) {
 		plugin = pbPlugin;
 		insigns = (InSigns) insignsPlugin;
+		format = new DecimalFormat("####.##");
 		
 		//Create changers for all player stats
 		for(final String stat : plugin.sql.sqlPlayers.statsList) {
 			String s = stat;
 			if(s.equals("teamattacks")) s = "ta";
-			if(s.equals("hitquote")) s = "hq";
-			if(s.equals("airstrikes")) s = "as";
-			if(s.equals("money_spent")) s = "spent";
+			else if(s.equals("hitquote")) s = "hq";
+			else if(s.equals("airstrikes")) s = "as";
+			else if(s.equals("money_spent")) s = "spent";
 			
-			insigns.addChanger(new Changer("[PB_"+s.toUpperCase()+"]", "paintball.insigns."+s) {
+			insigns.addChanger(new Changer("[PB_"+s.toUpperCase()+"]", "paintball.admin") {
 
 				@Override
 				public String getValue(Player player, Location location) {
@@ -34,16 +36,48 @@ public class InSignsFeature {
 					if(!plugin.sql.isConnected()) return Translator.getString("NOT_CONNECTED");
 					else if(plugin.pm.exists(playerName)) {
 						if(stat.equals("hitquote") || stat.equals("kd")) {
-							DecimalFormat dec = new DecimalFormat("####.##");
-							float statF = (float)(Integer)plugin.pm.getStats(playerName).get(stat) / 100;
-							return dec.format(statF);
-						} else return ""+plugin.pm.getStats(playerName).get(stat);
+							float statF = ((float)plugin.pm.getStats(playerName).get(stat)) / 100;
+							return format.format(statF);
+						} else return "" + plugin.pm.getStats(playerName).get(stat);
 					}
 					else return Translator.getString("NOT_FOUND");
 				}
 
 			});
+			
+			// rank changers:
+			insigns.addChanger(new Changer("[PB_R_"+s.toUpperCase()+"]", "paintball.admin") {
+
+				@Override
+				public String getValue(Player player, Location location) {
+					String playerName = player.getName();
+					if (!plugin.sql.isConnected())
+						return Translator.getString("NOT_CONNECTED");
+					else if (plugin.pm.exists(playerName)) {
+						return "" + plugin.stats.getRank(playerName, stat);
+					} else
+						return Translator.getString("NOT_FOUND");
+				}
+
+			});
 		}
+		
+		// Create additional changer for default rank (points):
+		insigns.addChanger(new Changer("[PB_RANK]", "paintball.admin") {
+
+			@Override
+			public String getValue(Player player, Location location) {
+				String playerName = player.getName();
+				if (!plugin.sql.isConnected())
+					return Translator.getString("NOT_CONNECTED");
+				else if (plugin.pm.exists(playerName)) {
+					return "" + plugin.stats.getRank(playerName, "points");
+				} else
+					return Translator.getString("NOT_FOUND");
+			}
+
+		});
+		
 	}
 
 }
