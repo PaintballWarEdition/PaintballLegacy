15cb23ee22bbb415b8f44aa2a1cfbd861432b17b
blablubbabc <lukas@wirsindwir.de>
2013-07-21 17:26:26 +0200

    VoteManager

--- src/de/blablubbabc/paintball/VoteManager.java
+++ src/de/blablubbabc/paintball/VoteManager.java
@@ -1,6 +1,7 @@
 package de.blablubbabc.paintball;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,7 +15,7 @@ import de.blablubbabc.paintball.utils.Utils;
 public class VoteManager {
 
 	private final List<VoteOption> voteOptions = new ArrayList<VoteOption>();
-	private Map<String, Integer> playerVotes = new HashMap<String, Integer>();
+	private final Map<String, VoteOption> playerVotes = new HashMap<String, VoteOption>();
 	
 	
 	public VoteManager(int numberOfOptions, boolean addRandomOption) {
@@ -49,15 +50,62 @@ public class VoteManager {
 			player.sendMessage(Translator.getString("GAME_VOTE_NOT_VALID_ID", new KeyValuePair("max", String.valueOf(voteOptions.size()))));
 			return;
 		}
-		String playerName = player.getName();
-		Integer oldVote = playerVotes.get(playerName);
 		
-		//TODO ...
+		VoteOption vote = voteOptions.get(voteID);
+		
+		String playerName = player.getName();
+		VoteOption oldVote = playerVotes.get(playerName);
+		
+		if (oldVote != null) {
+			oldVote.removeVote();
+		}
+		
+		vote.addVote();
 	}
 	
 	// returns the highest voted AND currently ready arena. If no arena is ready -> return null
 	public String getArena() {
+		List<VoteOption> copy = new ArrayList<VoteOption>(voteOptions);
+		Collections.sort(copy);
 		
+		// get the currently ready arenas:
+		List<String> allReady = Paintball.instance.arenaManager.getReadyArenas();
+		
+		// start at highest voted option:
+		for (int i = copy.size() - 1; i >= 0; i--) {
+			VoteOption vote = copy.get(i);
+			String arenaName = vote.getArena();
+			// random vote option:
+			if (arenaName == null) {
+				List<String> remaining = new ArrayList<String>(allReady);
+				remaining.removeAll(getVoteAbleArenas());
+				// pick random:
+				if (remaining.size() > 0) {
+					return remaining.remove(Utils.random.nextInt(remaining.size()));
+				}
+				// still no arena found? -> go on and check the other vote-able ones
+				continue;
+			} else {
+				// check if arena is still ready:
+				if (allReady.contains(arenaName)) {
+					return arenaName;
+				}
+				// else -> keep searching:
+				continue;
+			}
+		}
+		
+		// still no ready arena found? -> return null
+		return null;
+	}
+	
+	public List<String> getVoteAbleArenas() {
+		List<String> arenas = new ArrayList<String>();
+		for (VoteOption option : voteOptions) {
+			String arenaName = option.getArena();
+			if (arenaName != null) arenas.add(arenaName);
+		}
+		return arenas;
 	}
 	
 	private class VoteOption implements Comparable<VoteOption> {
@@ -67,6 +115,14 @@ public class VoteManager {
 		
 		private VoteOption(String arenaName) {
 			this.arenaName = arenaName;
+		}
+		
+		public void removeVote() {
+			if (votes > 0) votes--;
+		}
+		
+		public void addVote() {
+			votes++;
 		}
 		
 		public String getArena() {
@@ -84,6 +140,5 @@ public class VoteManager {
 			}
 			return this.getVotes() - other.getVotes();
 		}
-		
 	}
 }
