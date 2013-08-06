a09b171eb02937194c077f6816acf2a6711bc111
blablubbabc <lukas@wirsindwir.de>
2013-08-07 00:34:07 +0200

    Changed frag message to show player concrete name instead of display name, due to problems with colored display names and other weird things, very depending on other plugins..

--- src/de/blablubbabc/paintball/Origin.java
+++ src/de/blablubbabc/paintball/Origin.java
@@ -13,11 +13,11 @@ public class Origin {
 	
 	protected Map<String, String> getDefaultVariablesMap(FragInformations fragInfo) {
 		Map<String, String> vars = new HashMap<String, String>();
-		vars.put("killer", fragInfo.getKiller().getDisplayName());
+		vars.put("killer", fragInfo.getKiller().getName());
 		vars.put("killer_color", fragInfo.getKillerColor().toString());
 		vars.put("pre_killer", fragInfo.getPreKiller());
 		vars.put("after_killer", fragInfo.getAfterKiller());
-		vars.put("target", fragInfo.getTarget().getDisplayName());
+		vars.put("target", fragInfo.getTarget().getName());
 		vars.put("target_color", fragInfo.getTargetColor().toString());
 		vars.put("pre_target", fragInfo.getPreTarget());
 		vars.put("after_target", fragInfo.getAfterTarget());
