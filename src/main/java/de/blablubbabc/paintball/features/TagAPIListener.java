a5ace04fc44a3bd550b4355f21c7af484a9967cb
blablubbabc <lukas@wirsindwir.de>
2020-06-29 19:59:04 +0200

    Minor formatting.

--- src/main/java/de/blablubbabc/paintball/features/TagAPIListener.java
+++ src/main/java/de/blablubbabc/paintball/features/TagAPIListener.java
@@ -26,44 +26,45 @@ public class TagAPIListener implements Listener {
 	public TagAPIListener(Paintball plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onTagRecieve(AsyncPlayerReceiveNameTagEvent event) {
 		this.handleAsyncEvent(event);
 	}
-	
+
 	private void handleAsyncEvent(final AsyncPlayerReceiveNameTagEvent event) {
-		// getting a players name tag can depend on many dynamic, internal things, which are currently not thread safe (yet)
-        if (!Thread.currentThread().equals(Paintball.getInstance().mainThread)) {
-            final Future<Boolean> future = Bukkit.getScheduler().callSyncMethod(Paintball.getInstance(), new Callable<Boolean>() {
-                @Override
-                public Boolean call() throws Exception {
+		// getting a players name tag can depend on many dynamic, internal things, which are currently not thread safe
+		// (yet)
+		if (!Thread.currentThread().equals(Paintball.getInstance().mainThread)) {
+			final Future<Boolean> future = Bukkit.getScheduler().callSyncMethod(Paintball.getInstance(), new Callable<Boolean>() {
+				@Override
+				public Boolean call() throws Exception {
 					handleEventSync(event);
-                    return true;
-                }
-            });
-            final long start = System.currentTimeMillis();
-            while (!future.isCancelled() && !future.isDone()) {
-                if ((Paintball.getInstance().mainThread == null) || ((System.currentTimeMillis() - start) > 5000)) {
-                    if (Paintball.getInstance().mainThread != null) {
-                        Log.severe("Tag handling took too long (limit: 5000ms). Ignoring for " + event.getNamedPlayer().getName() + " as seen by " + event.getPlayer().getName());
-                    }
-                    return;
-                }
-                try {
-                    Thread.sleep(5);
-                } catch (final InterruptedException ex) {
-                }
-            }
-            if (future.isCancelled()) {
-                Log.debug("Sync task for tag of " + event.getNamedPlayer().getName() + " to " + event.getPlayer().getName() + " was cancelled. Skipping.");
-                return;
-            }
-        } else {
-        	handleEventSync(event);
-        }
-    }
-	
+					return true;
+				}
+			});
+			final long start = System.currentTimeMillis();
+			while (!future.isCancelled() && !future.isDone()) {
+				if ((Paintball.getInstance().mainThread == null) || ((System.currentTimeMillis() - start) > 5000)) {
+					if (Paintball.getInstance().mainThread != null) {
+						Log.severe("Tag handling took too long (limit: 5000ms). Ignoring for " + event.getNamedPlayer().getName() + " as seen by " + event.getPlayer().getName());
+					}
+					return;
+				}
+				try {
+					Thread.sleep(5);
+				} catch (final InterruptedException ex) {
+				}
+			}
+			if (future.isCancelled()) {
+				Log.debug("Sync task for tag of " + event.getNamedPlayer().getName() + " to " + event.getPlayer().getName() + " was cancelled. Skipping.");
+				return;
+			}
+		} else {
+			handleEventSync(event);
+		}
+	}
+
 	private void handleEventSync(final AsyncPlayerReceiveNameTagEvent event) {
 		Player player = event.getPlayer();
 		Match match = plugin.matchManager.getMatch(player);
@@ -73,22 +74,21 @@ public class TagAPIListener implements Listener {
 				if (plugin.tagsInvis) {
 					// invisible tags:
 					event.setTag("§§§§");
-					
 				} else if (plugin.tagsColor) {
 					// colored tags:
 					Match matchTar = plugin.matchManager.getMatch(target);
 					if (match == matchTar && match.isSurvivor(target)) {
-						//change colors according to team
-						//target is red, blue or spec ?
-						if(match.isRed(target)) {
+						// change colors according to team
+						// target is red, blue or spec ?
+						if (match.isRed(target)) {
 							event.setTag(Lobby.RED.color() + target.getDisplayName());
-						} else if(match.isBlue(target)) {
+						} else if (match.isBlue(target)) {
 							event.setTag(Lobby.BLUE.color() + target.getDisplayName());
-						} else if(match.isSpec(target)) {
+						} else if (match.isSpec(target)) {
 							event.setTag(Lobby.SPECTATE.color() + target.getDisplayName());
 						}
 					} else {
-						if(plugin.tagsRemainingInvis) {
+						if (plugin.tagsRemainingInvis) {
 							// invisible tags:
 							event.setTag("§§§§");
 						} else {
@@ -101,8 +101,8 @@ public class TagAPIListener implements Listener {
 					// colored tags for spectators, else normal tags/no changes:
 					Match matchTar = plugin.matchManager.getMatch(target);
 					if (match == matchTar && match.isSurvivor(target)) {
-						//change colors according to team
-						//target is red, blue or spec ?
+						// change colors according to team
+						// target is red, blue or spec ?
 						if (match.isRed(target)) {
 							event.setTag(Lobby.RED.color() + target.getDisplayName());
 						} else if (match.isBlue(target)) {
