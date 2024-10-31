8a1f8d428e9e769d25d8a17fcae2171eb2fb593c
blablubbabc <lukas@wirsindwir.de>
2024-10-31 21:27:47 +0100

    Remove old database migration and unused name fetching logic

--- src/main/java/de/blablubbabc/BlaDB/BlaSQLite.java
+++ src/main/java/de/blablubbabc/BlaDB/BlaSQLite.java
@@ -6,25 +6,17 @@ package de.blablubbabc.BlaDB;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.UUID;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import de.blablubbabc.paintball.Paintball;
-import de.blablubbabc.paintball.statistics.player.PlayerStat;
 import de.blablubbabc.paintball.utils.Log;
-import de.blablubbabc.paintball.utils.Utils;
-import de.blablubbabc.paintball.utils.uuids.UUIDFetcher;
 
 public class BlaSQLite {
 
@@ -41,205 +33,10 @@ public class BlaSQLite {
 		// Version 1.3.0:
 		this.databaseFile = new File(plugin.getDataFolder(), "pbdata_130" + ".db");
 
-		// Pre-checks:
-		File oldDBFile = new File(plugin.getDataFolder(), "pbdata_110" + ".db");
-		if (oldDBFile.exists()) {
-			if (Paintball.getInstance().uuidFirstRun) {
-				Log.warning("Detected first run with old database file: new configuration options were generated.", true);
-				Log.info("Stop the server, open the Paintball config, and make sure that the newly added 'UUID Conversion' settings are correctly set.", true);
-				Log.info("The next time you restart the server, the old paintball data will get imported into a new database file.", true);
-				Log.info("This process will take some while, in which the server will be unresponsive. You will get status reports in the console.", true);
-
-				// In case the plugin was loaded while the server is running:
-				for (Player admin : Bukkit.getOnlinePlayers()) {
-					if (admin.hasPermission("paintball.admin")) {
-						admin.sendMessage(ChatColor.RED + "Important! The next reload of Paintball will start a very slow uuid conversion and data import process.");
-						admin.sendMessage(ChatColor.RED + "Please view the server log now for more information.");
-					}
-				}
-
-				aborted = true;
-				return;
-			}
-
-			if (databaseFile.exists()) {
-				Log.severe("Cannot merge data from old database file ('pbdata_110.db') into already existing new database file ('pbdata_130.db').", true);
-				Log.severe("To properly import old data: Stop the server, delete the 'pbdata_130.db' file and then restart the server.", true);
-
-				aborted = true;
-				return;
-			}
-		}
-
 		// This creates the database file:
 		sqlArenaLobby = new SQLArenaLobby(this);
 		sqlPlayers = new SQLPlayers(this);
 		sqlGeneralStats = new SQLGeneralStats(this);
-
-		// Import data from old db (version 1.1.0):
-		if (oldDBFile.exists()) {
-			Log.info("Importing data from old database file 'pbdata_110.db'. This may take a while. Do not abort.");
-			// Attach old db:
-			this.updateQuery("ATTACH '" + oldDBFile.getAbsolutePath() + "' AS oldDB;");
-
-			Log.info("Importing lobby and arenas ...");
-
-			String arenasColumns = "name, active";
-			String arenaSettingsColumns = "balls, airstrikes, name, lives, respawns, round_time, grenades";
-			String arenaStatsColumns = "airstrikes, name, shots, kills, rounds, grenades";
-			String locationsColumns = "id, world, x, y, z, yaw, pitch";
-			String arenaSpawnsColumns = "arena, location_id";
-			String lobbySpawnsColumns = "location_id";
-			String generalStatsColumns = "key, value";
-
-			this.updateQuery("INSERT OR IGNORE INTO arenas (" + arenasColumns + ") SELECT " + arenasColumns + " FROM oldDB.arenas;");
-			this.updateQuery("INSERT OR IGNORE INTO arenasettings (" + arenaSettingsColumns + ") SELECT " + arenaSettingsColumns + " FROM oldDB.arenasettings;");
-			this.updateQuery("INSERT OR IGNORE INTO arenastats (" + arenaStatsColumns + ") SELECT " + arenaStatsColumns + " FROM oldDB.arenastats;");
-			this.updateQuery("INSERT OR IGNORE INTO locations (" + locationsColumns + ") SELECT " + locationsColumns + " FROM oldDB.locations;");
-			this.updateQuery("INSERT OR IGNORE INTO redspawns (" + arenaSpawnsColumns + ") SELECT " + arenaSpawnsColumns + " FROM oldDB.redspawns;");
-			this.updateQuery("INSERT OR IGNORE INTO bluespawns (" + arenaSpawnsColumns + ") SELECT " + arenaSpawnsColumns + " FROM oldDB.bluespawns;");
-			this.updateQuery("INSERT OR IGNORE INTO specspawns (" + arenaSpawnsColumns + ") SELECT " + arenaSpawnsColumns + " FROM oldDB.specspawns;");
-			this.updateQuery("INSERT OR IGNORE INTO lobbyspawns (" + lobbySpawnsColumns + ") SELECT " + lobbySpawnsColumns + " FROM oldDB.lobbyspawns;");
-
-			Log.info("Importing general statistics ...");
-			this.updateQuery("INSERT OR REPLACE INTO general_stats (" + generalStatsColumns + ") SELECT " + generalStatsColumns + " FROM oldDB.general_stats;");
-
-			Log.info("Loading player names ...");
-			List<String> playerNames = new ArrayList<String>();
-			Result r = this.resultQuery("SELECT name FROM oldDB.players;");
-			ResultSet rs = r.getResultSet();
-			try {
-				if (rs != null) {
-					while (rs.next()) {
-						playerNames.add(rs.getString(1));
-					}
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				r.close();
-			}
-
-			Log.info("Fetching player uuids...");
-			Map<String, UUID> localUUIDs;
-			Map<String, UUID> fetchedUUIDs;
-			try {
-				UUIDFetcher fetcher = new UUIDFetcher(playerNames);
-				localUUIDs = fetcher.searchLocal();
-				fetchedUUIDs = fetcher.fetch();
-			} catch (Exception e) {
-				e.printStackTrace();
-				aborted = true;
-				return;
-			}
-
-			Log.info("Importing player statistics ...");
-
-			StringBuilder oldColBuilder = new StringBuilder("name");
-			for (PlayerStat stat : PlayerStat.values()) {
-				String key = stat.getKey();
-				oldColBuilder.append(',').append(key);
-			}
-			String oldPlayerColumns = oldColBuilder.toString();
-			String newPlayerColumns = "uuid," + oldPlayerColumns;
-
-			int counter = 0;
-			List<String> unconverted = new ArrayList<String>();
-
-			// Start transaction:
-			this.updateQuery("BEGIN IMMEDIATE TRANSACTION;");
-			for (String playerName : playerNames) {
-				UUID uuid = localUUIDs.get(playerName);
-				if (uuid == null) {
-					uuid = fetchedUUIDs.get(playerName);
-					if (uuid == null) {
-						unconverted.add(playerName);
-						continue;
-					}
-				}
-
-				// Import player statistics:
-
-				// Check if we already have statistics for this player in the new database:
-				Map<PlayerStat, Integer> playerStats = sqlPlayers.getPlayerStats(uuid);
-				if (!playerStats.isEmpty()) {
-					Log.warning("Found multiple names for player '" + playerName + "'. Merging statistics.");
-					// The player has multiple names (for example because they already changed their
-					// name).
-					// Merging other old statistics:
-					Result oldStatsResult = this.resultQuery("SELECT * FROM oldDB.players WHERE name='" + playerName + "' LIMIT 1;");
-					ResultSet oldStatsRS = oldStatsResult.getResultSet();
-					try {
-						if (oldStatsRS != null && oldStatsRS.next()) {
-							for (PlayerStat stat : PlayerStat.values()) {
-								int curValue = playerStats.get(stat);
-								playerStats.put(stat, curValue + oldStatsRS.getInt(stat.getKey()));
-							}
-						}
-					} catch (SQLException e) {
-						e.printStackTrace();
-					} finally {
-						oldStatsResult.close();
-					}
-
-					// Recalculate certain statistic values:
-					playerStats.put(PlayerStat.HITQUOTE, Utils.calculateQuote(playerStats.get(PlayerStat.HITS), playerStats.get(PlayerStat.SHOTS)));
-					playerStats.put(PlayerStat.KD, Utils.calculateQuote(playerStats.get(PlayerStat.KILLS), playerStats.get(PlayerStat.DEATHS)));
-
-					// Save merged stats:
-					sqlPlayers.setPlayerStats(uuid, playerStats);
-
-					// Update player name, because of the assumption that the name which was added
-					// later to the database, and is therefore at the end of the playerNames list,
-					// is the latest name for the player:
-					this.updateQuery("UPDATE OR IGNORE players SET name='" + playerName + "' WHERE uuid='" + uuid.toString() + "';");
-				} else {
-					// insert old player stats:
-					this.updateQuery("INSERT OR IGNORE INTO players (" + newPlayerColumns + ") SELECT \""
-							+ uuid.toString() + "\"," + oldPlayerColumns + " FROM oldDB.players WHERE name='" + playerName + "';");
-				}
-
-				// Give feedback about the progress, and flush data:
-				counter++;
-				if ((counter % 1000) == 0) {
-					Log.info("Progress: " + counter);
-				}
-			}
-
-			// Commit:
-			Log.info("Saving changes to disk...");
-			this.updateQuery("END TRANSACTION;");
-
-			// Detach old db:
-			this.updateQuery("DETACH oldDB;");
-
-			Log.logColored(ChatColor.GREEN + "Done!");
-
-			// Rename old db file:
-			final File backupDBFile = new File(plugin.getDataFolder(), "pbdata_110-backup" + ".db");
-			oldDBFile.renameTo(backupDBFile);
-
-			// Inform about unconverted / not imported data:
-			File unconvertedConfigFile = new File(plugin.getDataFolder(), "unconvertedPlayers.yml");
-			if (unconvertedConfigFile.exists()) {
-				Log.warning("Removing old '" + unconvertedConfigFile.getName() + "' file.");
-				unconvertedConfigFile.delete();
-			}
-
-			if (!unconverted.isEmpty()) {
-				Log.warning("Some player statistics couldn't be imported, because we didn't find uuid's for them.", true);
-				Log.warning("Those players either have no Mojang account, or they have already changed their name.", true);
-				Log.warning("Their names were saved to the file '" + unconvertedConfigFile.getName() + "'.", true);
-
-				YamlConfiguration unconvertedConfig = YamlConfiguration.loadConfiguration(unconvertedConfigFile);
-				unconvertedConfig.set("unconverted players", unconverted);
-				try {
-					unconvertedConfig.save(unconvertedConfigFile);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
 	}
 
 	public void closeConnection() {
